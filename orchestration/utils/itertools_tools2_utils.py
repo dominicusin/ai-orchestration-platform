cb0fbc65832eee2cdb7136d2207d1240c3a6f35a
AI Pipeline Developer <dev@ai-pipeline.local>
2026-03-24 13:29:27 +0200

    feat: add itertools tools2 utilities
    - utils/itertools_tools2_utils.py: take_n, drop_n

---
+++ orchestration/utils/itertools_tools2_utils.py
@@ -0,0 +1,26 @@
+"""Itertools tools2 utilities"""
+
+import itertools
+from typing import List, Iterator, Callable
+
+
+def islice_iter(iterable: Iterator, stop: int) -> Iterator:
+    """Slice iterator"""
+    return itertools.islice(iterable, stop)
+
+
+def take_n(iterable: Iterator, n: int) -> List:
+    """Take n items"""
+    return list(itertools.islice(iterable, n))
+
+
+def drop_n(iterable: Iterator, n: int) -> Iterator:
+    """Drop n items"""
+    for i, item in enumerate(iterable):
+        if i >= n:
+            yield item
+
+
+def filter_none(iterable: Iterator) -> Iterator:
+    """Filter None values"""
+    return itertools.filterfalse(lambda x: x is None, iterable)
