28d6ebfbeb44e80ee13f29cebfbb9feb25096879
AI Pipeline Developer <dev@ai-pipeline.local>
2026-03-24 18:35:30 +0200

    feat: add itertools tools25 utilities
    - utils/itertools_tools25_utils.py: zip, any, all

---
+++ orchestration/utils/itertools_tools25_utils.py
@@ -0,0 +1,18 @@
+"""Itertools tools25 utilities"""
+
+import itertools
+
+
+def zip_2(*iterables):
+    """Zip iterables"""
+    return itertools.zip_longest(*iterables[:2]) if len(iterables) == 2 else zip(*iterables)
+
+
+def any_2(iterable):
+    """Any"""
+    return any(iterable)
+
+
+def all_2(iterable):
+    """All"""
+    return all(iterable)
