96204476b56ce47f86ea1d2c9184af1f03608747
AI Pipeline Developer <dev@ai-pipeline.local>
2026-03-24 13:26:56 +0200

    feat: update operator utilities
    - utils/operator_utils.py: attr_getter, item_getter

--- orchestration/utils/operator_utils.py
+++ orchestration/utils/operator_utils.py
@@ -1,49 +1,34 @@
 """Operator utilities"""
 
 import operator
-from typing import Callable, Any
+from typing import Any
 
 
-def attrgetter(attr: str) -> Callable:
-    """Get attribute from object"""
-    return operator.attrgetter(attr)
+def attr_getter(name: str):
+    """Get attribute"""
+    return operator.attrgetter(name)
 
 
-def itemgetter(item: Any) -> Callable:
-    """Get item from object"""
-    return operator.itemgetter(item)
+def item_getter(index: Any):
+    """Get item"""
+    return operator.itemgetter(index)
 
 
-def methodcaller(method: str, *args, **kwargs) -> Callable:
-    """Call method on object"""
-    return operator.methodcaller(method, *args, **kwargs)
+def method_caller(name: str, *args):
+    """Call method"""
+    return operator.methodcaller(name, *args)
 
 
-def lt(a: Any, b: Any) -> bool:
+def lt_op(a: Any, b: Any) -> bool:
     """Less than"""
     return operator.lt(a, b)
 
 
-def gt(a: Any, b: Any) -> bool:
+def gt_op(a: Any, b: Any) -> bool:
     """Greater than"""
     return operator.gt(a, b)
 
 
-def eq(a: Any, b: Any) -> bool:
+def eq_op(a: Any, b: Any) -> bool:
     """Equal"""
     return operator.eq(a, b)
-
-
-def ne(a: Any, b: Any) -> bool:
-    """Not equal"""
-    return operator.ne(a, b)
-
-
-def is_(a: Any, b: Any) -> bool:
-    """Is same object"""
-    return operator.is_(a, b)
-
-
-def is_not(a: Any, b: Any) -> bool:
-    """Is not same object"""
-    return operator.is_not(a, b)
