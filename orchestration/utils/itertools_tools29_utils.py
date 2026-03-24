4d4deddc748f5b829636947464040eebabf22fc6
AI Pipeline Developer <dev@ai-pipeline.local>
2026-03-24 18:39:00 +0200

    feat: add itertools tools29 utilities
    - utils/itertools_tools29_utils.py: sum, list

---
+++ orchestration/utils/itertools_tools29_utils.py
@@ -0,0 +1,13 @@
+"""Itertools tools29 utilities"""
+
+import itertools
+
+
+def sum_2(iterable, start=0):
+    """Sum"""
+    return sum(iterable, start)
+
+
+def list_2(iterable):
+    """List"""
+    return list(iterable)
