d9e1c08fddd56cf75df86700f8363b781aa1054d
AI Pipeline Developer <dev@ai-pipeline.local>
2026-03-24 18:45:35 +0200

    feat: add itertools tools30 utilities
    - utils/itertools_tools30_utils.py: tuple, set

---
+++ orchestration/utils/itertools_tools30_utils.py
@@ -0,0 +1,13 @@
+"""Itertools tools30 utilities"""
+
+import itertools
+
+
+def tuple_2(iterable):
+    """Tuple"""
+    return tuple(iterable)
+
+
+def set_2(iterable):
+    """Set"""
+    return set(iterable)
