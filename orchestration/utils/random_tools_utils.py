511c32b564075a9cb065ac4f68f0cf6ae3488a4a
AI Pipeline Developer <dev@ai-pipeline.local>
2026-03-24 19:44:06 +0200

    feat: add random tools utilities
    - utils/random_tools_utils.py: random, randint

---
+++ orchestration/utils/random_tools_utils.py
@@ -0,0 +1,13 @@
+"""Random tools utilities"""
+
+import random
+
+
+def random_2():
+    """Random"""
+    return random.random()
+
+
+def randint_2(a, b):
+    """Randint"""
+    return random.randint(a, b)
