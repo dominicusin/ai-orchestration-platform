245b2241d273928f91a386967a787b9f223cbd7b
AI Pipeline Developer <dev@ai-pipeline.local>
2026-03-24 15:16:53 +0200

    feat: add itertools tools14 utilities
    - utils/itertools_tools14_utils.py: islice, take

---
+++ orchestration/utils/itertools_tools14_utils.py
@@ -0,0 +1,14 @@
+"""Itertools tools14 utilities"""
+
+import itertools
+from typing import Iterator
+
+
+def islice_3(iterable, stop):
+    """Islice"""
+    return itertools.islice(iterable, stop)
+
+
+def take_2(iterable, n):
+    """Take n items"""
+    return itertools.islice(iterable, n)
