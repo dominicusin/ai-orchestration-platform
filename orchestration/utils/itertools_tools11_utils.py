9a923af2df7ba25f15fcdfaba8e345fcec52c16a
AI Pipeline Developer <dev@ai-pipeline.local>
2026-03-24 15:10:48 +0200

    feat: add itertools tools11 utilities
    - utils/itertools_tools11_utils.py: zip_longest, accumulate

---
+++ orchestration/utils/itertools_tools11_utils.py
@@ -0,0 +1,14 @@
+"""Itertools tools11 utilities"""
+
+import itertools
+from typing import Iterator
+
+
+def zip_longest_2(*iterables, fillvalue=None) -> Iterator:
+    """Zip longest"""
+    return itertools.zip_longest(*iterables, fillvalue=fillvalue)
+
+
+def accumulate_2(iterable, func=None) -> Iterator:
+    """Accumulate"""
+    return itertools.accumulate(iterable, func) if func else itertools.accumulate(iterable)
