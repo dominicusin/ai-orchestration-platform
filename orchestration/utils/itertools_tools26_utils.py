ae40c14b8ecefbab65ecc6216e9dd5323ee4de8d
AI Pipeline Developer <dev@ai-pipeline.local>
2026-03-24 18:36:31 +0200

    feat: add itertools tools26 utilities
    - utils/itertools_tools26_utils.py: next, len

---
+++ orchestration/utils/itertools_tools26_utils.py
@@ -0,0 +1,13 @@
+"""Itertools tools26 utilities"""
+
+import itertools
+
+
+def next_2(iterator, default=None):
+    """Next from iterator"""
+    return next(iterator, default)
+
+
+def len_2(obj):
+    """Len"""
+    return len(obj)
