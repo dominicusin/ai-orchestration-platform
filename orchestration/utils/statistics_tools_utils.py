89f58deafecd814fc676d84c32c13d3e61281e94
AI Pipeline Developer <dev@ai-pipeline.local>
2026-03-24 19:18:01 +0200

    feat: add statistics tools utilities
    - utils/statistics_tools_utils.py: mean, median

---
+++ orchestration/utils/statistics_tools_utils.py
@@ -0,0 +1,13 @@
+"""Statistics tools utilities"""
+
+import statistics
+
+
+def mean_2(data):
+    """Mean"""
+    return statistics.mean(data)
+
+
+def median_2(data):
+    """Median"""
+    return statistics.median(data)
