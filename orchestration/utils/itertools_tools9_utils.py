e45eb9b1ef9ae3d5621c9be75053896824dc0a1e
AI Pipeline Developer <dev@ai-pipeline.local>
2026-03-24 15:09:28 +0200

    feat: add itertools tools9 utilities
    - utils/itertools_tools9_utils.py: filterfalse, takewhile, dropwhile

---
+++ orchestration/utils/itertools_tools9_utils.py
@@ -0,0 +1,19 @@
+"""Itertools tools9 utilities"""
+
+import itertools
+from typing import Iterator
+
+
+def filterfalse_2(predicate, iterable: Iterator) -> Iterator:
+    """Filter false"""
+    return itertools.filterfalse(predicate, iterable)
+
+
+def takewhile_2(predicate, iterable: Iterator) -> Iterator:
+    """Take while"""
+    return itertools.takewhile(predicate, iterable)
+
+
+def dropwhile_2(predicate, iterable: Iterator) -> Iterator:
+    """Drop while"""
+    return itertools.dropwhile(predicate, iterable)
