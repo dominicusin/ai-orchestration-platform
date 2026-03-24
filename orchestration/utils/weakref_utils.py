a11141ad9fed98577e8c9762761d9533f48867ab
AI Pipeline Developer <dev@ai-pipeline.local>
2026-03-24 03:34:48 +0200

    feat: add weakref utilities
    - utils/weakref_utils.py: make_weakref, weak_cache

---
+++ orchestration/utils/weakref_utils.py
@@ -0,0 +1,43 @@
+"""Weakref utilities"""
+
+import weakref
+import functools
+from typing import Any, Callable
+
+
+def make_weakref(obj: Any):
+    """Create weak reference"""
+    return weakref.ref(obj)
+
+
+def get_weakref(obj: Any):
+    """Get weak reference"""
+    return weakref.ref(obj)
+
+
+def weak_callback(callback: Callable):
+    """Create weak callback"""
+    return weakref.finalize
+
+
+class WeakMethod:
+    """Weak method reference"""
+    
+    def __init__(self, method: Callable):
+        self.ref = weakref.Method(method)
+    
+    def __call__(self):
+        return self.ref()
+
+
+def weak_cache(func: Callable) -> Callable:
+    """Weak reference cache"""
+    cache = weakref.WeakValueDictionary()
+    
+    @functools.wraps(func)
+    def wrapper(*args):
+        key = str(args)
+        if key not in cache:
+            cache[key] = func(*args)
+        return cache[key]
+    return wrapper
