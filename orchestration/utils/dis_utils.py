f5526965e0af96122143268e42e68409b84e0ac4
AI Pipeline Developer <dev@ai-pipeline.local>
2026-03-24 03:36:13 +0200

    feat: add dis utilities
    - utils/dis_utils.py: disassemble, bytecode_info

---
+++ orchestration/utils/dis_utils.py
@@ -0,0 +1,28 @@
+"""Dis utilities"""
+
+import dis
+from typing import Callable
+
+
+def disassemble(func: Callable) -> str:
+    """Disassemble function"""
+    import io
+    output = io.StringIO()
+    dis.dis(func, file=output)
+    return output.getvalue()
+
+
+def get_bytecode(func: Callable) -> list:
+    """Get bytecode instructions"""
+    return list(dis.get_instructions(func))
+
+
+def bytecode_info(func: Callable) -> dict:
+    """Get bytecode info"""
+    instructions = dis.get_instructions(func)
+    return {
+        "argcount": func.__code__.co_argcount,
+        "varnames": func.__code__.co_varnames,
+        "names": func.__code__.co_names,
+        "instructions": [str(i) for i in instructions]
+    }
