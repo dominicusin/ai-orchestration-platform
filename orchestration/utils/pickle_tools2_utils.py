3223c51a8c5a8b5263bde672a88da5db6fce84bb
AI Pipeline Developer <dev@ai-pipeline.local>
2026-03-25 15:31:25 +0200

    feat: add pickle tools2 utilities
    - utils/pickle_tools2_utils.py: pickle_dumps, pickle_loads

---
+++ orchestration/utils/pickle_tools2_utils.py
@@ -0,0 +1,13 @@
+"""Pickle tools2 utilities"""
+
+import pickle
+
+
+def pickle_dumps_2(obj):
+    """Pickle dumps"""
+    return pickle.dumps(obj)
+
+
+def pickle_loads_2(data):
+    """Pickle loads"""
+    return pickle.loads(data)
