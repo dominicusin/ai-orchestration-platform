8d469628af1daf89631045b9c2a93731ca29d661
AI Pipeline Developer <dev@ai-pipeline.local>
2026-03-25 15:30:35 +0200

    feat: add json tools3 utilities
    - utils/json_tools3_utils.py: json_dumps, json_loads

---
+++ orchestration/utils/json_tools3_utils.py
@@ -0,0 +1,13 @@
+"""JSON tools3 utilities"""
+
+import json
+
+
+def json_dumps_2(obj):
+    """JSON dumps"""
+    return json.dumps(obj)
+
+
+def json_loads_2(s):
+    """JSON loads"""
+    return json.loads(s)
