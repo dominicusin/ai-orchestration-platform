a81d19c06fb15be65b2451ead29cb0aec177183a
AI Pipeline Developer <dev@ai-pipeline.local>
2026-03-25 06:49:45 +0200

    feat: add statistics tools5 utilities
    - utils/statistics_tools5_utils.py: mode, stdev

---
+++ orchestration/utils/statistics_tools5_utils.py
@@ -0,0 +1,13 @@
+"""Statistics tools5 utilities"""
+
+import statistics
+
+
+def mode_3(data):
+    """Mode"""
+    return statistics.mode(data)
+
+
+def stdev_3(data):
+    """Stdev"""
+    return statistics.stdev(data)
